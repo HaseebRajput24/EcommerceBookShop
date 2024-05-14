a05552acf3075085f961d742d6e5b87235e84dce
Haseeb <haseebrao1996@gmail.com>
2024-05-14 08:46:59 -0700

    DataBase connection using Entityframework Core

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceBookShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceBookShop.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
+        { 
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using EcommerceBookShop.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
